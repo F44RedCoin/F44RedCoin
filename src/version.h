864461d623983653b1dad689ff5ce9a198ad538f
F44RedCoin <73239873+F44RedCoin@users.noreply.github.com>
2020-10-21 11:08:35 -0400

    Update version.h

--- src/version.h
+++ src/version.h
@@ -9,13 +9,13 @@
  * network protocol versioning
  */
 
-static const int PROTOCOL_VERSION = 70015;
+static const int PROTOCOL_VERSION = 70016;
 
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
 //! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
+static const int GETHEADERS_VERSION = 70016;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
@@ -40,6 +40,6 @@ static const int FEEFILTER_VERSION = 70013;
 static const int SHORT_IDS_BLOCKS_VERSION = 70014;
 
 //! not banning for invalid compact blocks starts with this version
-static const int INVALID_CB_NO_BAN_VERSION = 70015;
+static const int INVALID_CB_NO_BAN_VERSION = 70016;
 
 #endif // F44REDCOIN_VERSION_H
